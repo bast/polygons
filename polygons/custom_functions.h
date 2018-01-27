cae24a2bd4523be11358c7f7d8070d4d3ae7c6b2
Radovan Bast <bast@users.noreply.github.com>
2018-01-27 17:22:37 +0100

    nope, we don't want inline in this case

--- polygons/custom_functions.h
+++ polygons/custom_functions.h
@@ -1,8 +1,6 @@
 #pragma once
 
-// FIXME consider inline
 // FIXME generalize to an array of parameters
 double h_function(const double a);
 
-// FIXME consider inline
 double g_function(const double distance);

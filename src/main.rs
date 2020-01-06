5857fed5a171ddfc6c58d6c57acfd81b83551e30
Radovan Bast <bast@users.noreply.github.com>
2020-01-06 10:48:32 +0100

    time both tree build and run

--- src/main.rs
+++ src/main.rs
@@ -27,8 +27,6 @@ fn run_benchmark() {
         polygons.push(polygon);
     }
 
-    let tree = polygons::build_tree(&polygons, 16, 16);
-
     let (x_min, x_max) = (-1.0, (num_blocks - 1) as f64 * offset + 2.0);
     let (y_min, y_max) = (-1.0, 2.0);
 
@@ -44,12 +42,14 @@ fn run_benchmark() {
     }
 
     let start = Instant::now();
+    let tree = polygons::build_tree(&polygons, 16, 16);
+    println!("time elapsed in building tree: {:?}", start.elapsed());
+
+    let start = Instant::now();
     let _distances = polygons::get_distances_edge(&tree, &reference_points);
     let (_indices, _distances) = polygons::get_distances_vertex(&tree, &reference_points);
     let _contains = polygons::contains_points(&tree, &reference_points);
-    let duration = start.elapsed();
-
-    println!("time elapsed in benchmark: {:?}", duration);
+    println!("time elapsed in benchmark: {:?}", start.elapsed());
 }
 
 fn main() {

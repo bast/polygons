f5fcf71a22ee07a8ffbd39896fd69f3ea26aaf58
Radovan Bast <bast@users.noreply.github.com>
2020-03-26 11:16:45 +0100

    make src/lib.rs easier to read

--- src/lib.rs
+++ src/lib.rs
@@ -5,11 +5,14 @@ mod point;
 mod tree;
 
 pub use crate::io::read_vector;
+
 pub use crate::point::Point;
-pub use crate::tree::build_tree;
-pub use crate::tree::contains_points;
-pub use crate::tree::create_polygon;
-pub use crate::tree::distances_nearest_edges;
-pub use crate::tree::distances_nearest_vertices;
 pub use crate::tree::Edge;
 pub use crate::tree::Node;
+
+pub use crate::tree::build_tree;
+pub use crate::tree::create_polygon;
+
+pub use crate::tree::contains_points;
+pub use crate::tree::distances_nearest_edges;
+pub use crate::tree::distances_nearest_vertices;

f71a4e0ed075dcf308a7d6db6007a14ee29daa34
Radovan Bast <bast@users.noreply.github.com>
2021-02-26 14:10:50 +0100

    script to visually verify the inside/outside part

--- tests/plot/plot.py
+++ tests/plot/plot.py
@@ -1,8 +1,11 @@
-import matplotlib.pyplot as plt
 import sys
+import random
+import click
+import matplotlib.pyplot as plt
+import polygons
 
 
-def read_data(file_name):
+def read_polygons(file_name):
     polygons = []
     points = []
     with open(file_name, "r") as f:
@@ -18,30 +21,57 @@ def read_data(file_name):
     return polygons
 
 
-def parse_command_line():
-
-    if len(sys.argv) != 3:
-        sys.stderr.write(
-            "Usage: {0} [in-txt-file] [out-png-file]\n".format(sys.argv[0])
-        )
-        sys.exit(1)
-
-    in_file_name = sys.argv[1]
-    out_file_name = sys.argv[2]
-
-    return in_file_name, out_file_name
-
-
-if __name__ == "__main__":
-    in_file_name, out_file_name = parse_command_line()
-
-    polygons = read_data(in_file_name)
-
+@click.command()
+@click.option("--polygons-file", required=True, help="File name containing polygons.")
+@click.option("--figure", required=True, help="Output file name.")
+def main(polygons_file, figure):
     plt.figure()
     plt.gca().set_aspect("equal")
 
-    for polygon in polygons:
-        x, y = zip(*polygon)
-        plt.plot(x, y, "b-", markersize=0.2, linewidth=0.3)
+    large_number = sys.float_info.max
+    xmin = large_number
+    xmax = -large_number
+    ymin = large_number
+    ymax = -large_number
 
-    plt.savefig(out_file_name, dpi=300)
+    ps = read_polygons(polygons_file)
+
+    for polygon in ps:
+        xs, ys = zip(*polygon)
+        plt.plot(xs, ys, "b-", markersize=0.2, linewidth=0.3)
+        xmin = min(xmin, min(xs))
+        xmax = max(xmax, max(xs))
+        ymin = min(ymin, min(ys))
+        ymax = max(ymax, max(ys))
+
+    num_edges_children = 4
+    num_nodes_children = 4
+    tree = polygons.build_tree(ps, num_edges_children, num_nodes_children)
+
+    num_points = 50000
+    points = [
+        (random.uniform(xmin, xmax), random.uniform(ymin, ymax))
+        for _ in range(num_points)
+    ]
+    inside = polygons.points_are_inside(tree, points)
+
+    xs_inside = []
+    ys_inside = []
+    xs_outside = []
+    ys_outside = []
+    for (i, (xs, ys)) in enumerate(points):
+        if inside[i]:
+            xs_inside.append(xs)
+            ys_inside.append(ys)
+        else:
+            xs_outside.append(xs)
+            ys_outside.append(ys)
+
+    plt.scatter(xs_inside, ys_inside, s=0.01, color="red")
+    plt.scatter(xs_outside, ys_outside, s=0.01, color="blue")
+
+    plt.savefig(figure, dpi=1200)
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/plot/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+python plot.py --polygons-file ../islands.txt --figure example.png
